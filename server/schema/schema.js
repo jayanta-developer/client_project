2b4bca3a9e302c78bde82f816b4202e9bebfa20f
jayanta-musquare <116143724+jayanta-musquare@users.noreply.github.com>
2022-10-21 00:41:07 +0530

    Hade a problem to perfrom query

---
+++ server/schema/schema.js
@@ -0,0 +1,40 @@
+const { client, tours } = require("../data");
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLSchema,
+} = require("graphql");
+
+//client
+const ClientType = new GraphQLObjectType({
+  name: "client",
+  fields: () => ({
+    _id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    role: { type: GraphQLString },
+    active: { type: GraphQLBoolean },
+    photo: { type: GraphQLString },
+    password: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return client.find((client) => client._id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
